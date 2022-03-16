f6ed893e88e3fd501a81a6639964cf8736fd660f
NickGreenSF <nicholasgreensf@gmail.com>
2022-03-15 19:02:29 -0700

    it runs

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Puzzle from './components/Puzzle'
+
+export default function App () {
+    return <Puzzle />
+}
---
+++ components/Puzzle.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+let mockdata : Array<String> = [
+    "Harry Potter and the Half-Blood Prince",
+    "Daniel Radcliffe",
+    "Rupert Grint",
+    "Emma Watson",
+    "Helena Bonham Carter",
+    "Robbie Coltrane",
+    "Warwick Davis"
+]
+
+export default function Puzzle () {
+    return <div>running</div>
+}
